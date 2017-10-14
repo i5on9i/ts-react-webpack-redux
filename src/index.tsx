0a598e17f12b1bedf92c3049f08bd942d48d5fda
i5on9i <gaedduck@gmail.com>
2017-10-14 12:56:40 +0900

    - add store - Provider is applied

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,22 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { HashRouter } from 'react-router-dom'
 
-// import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
-// import {todoApp} from './reducers/todoApp'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import {todoApp} from './reducers/todoApp'
 
 import SignUp from "./components/SignUp";
 
 
-
-// let store = createStore(todoApp)
+let store = createStore(todoApp)
 
 document.addEventListener("DOMContentLoaded", function() {
     ReactDOM.render(
-        // <Provider store={store}>
+        <Provider store={store}>
             <HashRouter>
                 <SignUp />
             </HashRouter>
-        // </Provider>
+        </Provider>
         ,
         document.getElementById('example'));
 });
